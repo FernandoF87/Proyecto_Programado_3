a87f18fef441b5edb05414d506d42a2bc7738388
FernandoF87 <flores.fernando@users.noreply.github.com>
2020-08-05 17:54:43 -0600

    Agregada clase Machine
    Define el comportamiento de la máquina (añadir dinero, dar vuelto, generar los papeles)

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ def load_img(name):
         return "Error"
 
 class Coin():
-    def __init__(self, master, x, y, value, change, label):
+    def __init__(self, master, x, y, value, change, label, money, machine):
         self.master = master
         self.label = label
+        self.machine = machine
+        self.money = money
         img = load_img("hundred.png")
         if not change:
-            self.button = Button(self.master, image=img, command= self.create_thread, relief=FLAT)
+            self.button = Button(self.master, image=img, command= self.create_thread, relief=FLAT, width=40, height=70, bg="#6AE1FF")
         else:
-            self.button = Button(self.master, image=img, command= self.erase, relief=FLAT)
+            self.button = Button(self.master, image=img, command= self.erase, relief=FLAT, width=40, height=70, bg="#6AE1FF")
         self.button.image = img
         self.x = x
         self.y = y
@@ -39,33 +41,53 @@ class Coin():
         tempx = self.x
         tempy = self.y
         run = True
-        while run:
-            if self.y > 50:
-                self.y = self.y - 10
-                self.button.place(x=self.x, y=self.y)
-                time.sleep(0.03)
-            elif self.x > 50:
-                self.x = self.x - 10
-                self.button.place(x=self.x, y=self.y)
-                time.sleep(0.03)
-            else:
-                run = False
-                self.x = tempx
-                self.y = tempy
-                self.button.place(x=self.x, y=self.y)
-        money += self.value
-        self.label.config(text=str(money))
+        if money >= self.value:
+            while run:
+                if self.y > 270:
+                    self.y = self.y - 10
+                    self.button.place(x=self.x, y=self.y)
+                    time.sleep(0.03)
+                elif self.x > 80:
+                    self.x = self.x - 10
+                    self.button.place(x=self.x, y=self.y)
+                    time.sleep(0.03)
+                else:
+                    run = False
+                    self.x = tempx
+                    self.y = tempy
+                    self.button.place(x=self.x, y=self.y)
+            self.machine.add_money(self.value)
+            money -= self.value
+            self.money.config(text="Dinero disponible: ₡ " + str(money))
+        else:
+            print("Mirá capo estás limpio")
 
     def erase(self):
+        global money
+        money += self.value
+        self.money.config(text="Dinero disponible: ₡ " + str(money))
         self.button.destroy()
 
 
-def cambio(label):
-    global money
-    if money > 0:
-        money -= 100
-    label.config(text=str(money), bg="blue")
-    time.sleep(0.1)
+class Machine():
+    def __init__(self, master, cost, label, label_money):
+        self.master = master
+        self.money = 0
+        self.cost = cost
+        self.label = label
+        self.label_money = label_money
+
+    def add_money(self, value):
+        self.money += value
+        if self.money >= self.cost:
+            if self.money > self.cost:
+                self.cambio(self.money-self.cost)
+            self.money = 0
+            Toplevel(self.master)
+        self.label.config(text="₡" + str(self.money))
+
+    def cambio(self, value):
+        coin = Coin(self.master, 350, 330, value, True, self.label, self.label_money, self)
 
 def main():
     global money
@@ -82,10 +104,17 @@ def main():
     machine.place(x=0,y=0)
     user.place(x=500,y=0)
     
-    label = Label(root, text=str(money), bg="red")
-    label.place(x=550, y=0)
+    label = Label(machine, text="₡ 0", bg="blue", width=18, height=3, font=("Haettenschweiler", 22))
+    label.place(x=60, y=89)
 
-    coin = Coin(root, 500, 350, 50, False, label)
+    label_money = Label(machine, text="Dinero disponible: ₡ 1000", bg="#6AE1FF", font=("Haettenschweiler", 16))
+    label_money.place(x=0, y=0)
+
+    maquina = Machine(root, 200, label, label_money)
+
+    coin = Coin(root, 520, 350, 50, False, label, label_money, maquina)
+    coin2 = Coin(root, 570, 350, 150, False, label, label_money, maquina)
+    coin3 = Coin(root, 620, 350, 250, False, label, label_money, maquina)
 
     root.mainloop()
 

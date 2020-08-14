2abf569007355a81c0da70de86183c7f17607727
FernandoF87 <flores.fernando@users.noreply.github.com>
2020-08-14 16:25:52 -0600

    Documentación interna

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import os, time, random
 from datetime import datetime, date
 from threading import Thread
 
+############################
+# Proyecto #3              #
+# Battle: Advice Machine   #
+# Anthony Chaves Achoy     #
+# Fenrnado Flores Moya     #
+############################
+
+# Declaración de variables globales
 on = True
 money = 1000
 language = True #Spanish as True, English as False
 
+
+#popup(message): Crea una pantalla popup para mostrar un mensaje específico
+    # E: mensaje
+    # S: Tk con el mensaje y un botón para cerrar
+    # R: -
 
 def popup(message):
     window = Tk()
@@ -23,13 +36,41 @@ def popup(message):
     boton.pack()
     window.mainloop()
 
+#load_img(name): Facilita la carga de imágenes desde la carpeta
+    # E: nombre de la imagen
+    # S: archivo de imagen
+    # R: String
+
 def load_img(name):
     if isinstance(name, str):
         path = os.path.join("imgs", name)
         img = PhotoImage(file=path)
         return img
     else:
-        return "Error"
+        print("Error")
+
+
+#clase Coin
+    #Atributos:
+        #master(Tk), x(numero), y(numero), label, money(Label), button(Botón), machine(objeto Machine), speed(numero)
+    #Metodos:
+        #__init__(master, x, y, value, change, label, money, machine)
+        #create_thread(): Crea un hilo para mover la imagen
+            #E: -
+            #S: Hilo que llama a move()
+            #R: -
+        #move(): Mueve la imagen hasta la ranura de la máquina y añade dinero
+            #E: -
+            #S: Movimiento de la imagen y cambia el dinero de la máquina y el disponible
+            #R: -
+        #erase(): Elimina la imagen y añade dinero al usuario
+            #E: -
+            #S: Se añade dinero al total disponible y el botón desaparece
+            #R: -
+        #change_lan(): Cambia el lenguaje en el que se muestra el dinero disponible
+            #E: -
+            #S: Cambia la configuración del texto del label que muestra el dinero disponible
+            #R: -
 
 class Coin():
     def __init__(self, master, x, y, value, change, label, money, machine):
@@ -110,6 +151,55 @@ class Coin():
             self.money.config(text="Dinero disponible: ₡ " + str(money))
         else:
             self.money.config(text="Available money: ₡ " + str(money))
+
+
+#clase Machine
+    #Atributos:
+        #master(Tk), money(numero), string(String), label, label_money(Label), costs(Lista de numeros), calidad(numero)
+        #calidades(Lista de numeros), seleccion(Lista de strings), arrow_pos(numero), label_arrow(Label), current_y(numero)
+        #button_up(Botón), button_down(Botón), button_enter(Botón), init(Boolean)
+    #Metodos:
+        #__init__(master, label, label_money)
+        #up_arrow(): Mueve la flecha hacia arriba
+            #E: -
+            #S: Cambia el valor de arrow_pos y current_y
+            #R: -
+        #down_arrow(): Mueve la flecha hacia abajo
+            #E: -
+            #S: Cambia el valor de arrow_pos y current_y
+            #R: -
+        #purchase(): Realiza la compra de un mensaje
+            #E: -
+            #S: Llama a create_thread para crear un mensaje, también llama a cambio() si hay vuelto disponible
+            #R: -
+        #create_thread(): Crea un hilo para animar el papel imprimiéndose
+            #E: -
+            #S: Hilo que llama a animation()
+            #R: -
+        #add_money(value): Añade dinero a la máquina
+            #E: -
+            #S: Añade el valor a money, determina la calidad basado en costs y la cantidad de dinero y actualiza labels
+            #R: -
+        #update_text(): Actualiza el idioma de la máquina
+            #E: -
+            #S: Actualiza los labels de los tipos de mensaje basado en el idioma
+            #R: -
+        #reset(): Reinicia la máquina después de una compra
+            #E: -
+            #S: Reinicia las variables de arrow_pos y current_y, tambien actualiza el label del display
+            #R: -
+        #cambio(value): Da vuelto al usuario
+            #E: -
+            #S: Crea un objeto Coin con valor value
+            #R: -
+        #change_lan(): Cambia el lenguaje en el que se muestra el display de la máquina
+            #E: -
+            #S: Cambia la configuración del texto del label que muestra los tipos de mensajes
+            #R: -
+        #animation(): Imprime el mensaje
+            #E: -
+            #S: Genera la animación de un papel saliendo de la ranura y crea un objeto Message correspondiente
+            #R: -
 
 class Machine():
     def __init__(self, master, label, label_money):
@@ -251,6 +341,26 @@ class Machine():
         else: 
             self.update_text()
 
+#clase Message
+    #Atributos:
+        #master(Tk), type(numero), quality(String), number(numero), pay(numero), change(numero), code(numero), monto(numero),
+        #title(String), bkgr(Imagen), t(numero), screen(TopLevel), bg(Canvas), msg(String), label_tit(Label), label_out(Label)
+        #date(String)
+    #Metodos:
+        #__init__(master, type, quality, payment, change)
+        #register_receipt(): Registra la factura basado en la fecha y hora
+            #E: -
+            #S: Establece la fecha y hora actual y llama a parse_receipt()
+            #R: -
+        #parse_receipt(): Parsea la factura basado en los datos del mensaje
+            #E: -
+            #S: Escribe la factura en facturas.txt
+            #R: -
+        #parse_message(): Obtiene y parsea el mensaje a mostrar
+            #E: -
+            #S: Frase en forma de string
+            #R: -
+
 class Message():
     def __init__(self, master, type, quality, payment, change):
         global language
@@ -269,7 +379,7 @@ class Message():
         else:
             self.code = random.randint(13,19)
             self.monto = 200
-        #"consejo", "dicho","chiste"
+
         if self.type == "consejo":
             if language:
                 self.title = "Consejo"
@@ -416,15 +526,21 @@ def main():
 
     label_money = Label(machine, text="Dinero disponible: ₡ 1000", bg="#6AE1FF", font=("Haettenschweiler", 16))
 
+    #Declaración de la máquina cuando está encendida
     if on:
         label.place(x=140, y=90)
         label_money.place(x=0, y=0)
         maquina = Machine(root, label, label_money)
 
+    #Declaración de las 3 monedas que puede utilizar el usuario
     coin = Coin(root, 520, 350, 25, False, label, label_money, maquina)
     coin2 = Coin(root, 570, 350, 50, False, label, label_money, maquina)
     coin3 = Coin(root, 620, 350, 100, False, label, label_money, maquina)
 
+    #change_lang(): Cambia el idioma en que se muestra la información en la máquina
+        #E: -
+        #S: Llama a los métodos change_lang de la máquina y las monedas
+        #R: -
     def change_lan():
         global language
         language = not language
@@ -436,7 +552,7 @@ def main():
             maquina.change_lan()
             coin.change_lan()
 
-
+    #Pantalla de administrador
     def pw_screen():
         admin_s = Toplevel(root)
         admin_s.geometry("500x350+250+200")
@@ -453,6 +569,7 @@ def main():
         pw.place(x=170, y=100)
 
         def functions():
+            #Botones correspondientes a las funciones de administrador
             if language:
                 end = Button(back_g, command=end_all, relief=FLAT, width=20, height=3, text="Apagar dispensador",bg="red", fg="white", font=("Fixedsys", 8))
                 cut = Button(back_g, command=cut_sales, relief=FLAT, width=20, height=3, text="Reiniciar ventas",bg="red", fg="white", font=("Fixedsys", 8))
@@ -466,10 +583,18 @@ def main():
             cut.place(x=170, y=170)
             report.place(x=170, y=240)
 
+        #end_all(): Apaga la máquina
+            #E: -
+            #S: Destruye los Tk de la pantalla principal y de la pantalla de administrador
+            #R: -
         def end_all():
             admin_s.destroy()
             root.destroy()
 
+        #cut_sales(): Reinicia el historial de ventas de los mensajes
+            #E: -
+            #S: Elimina las facturas de facturas.txt y reinicia las ventas de mensajes.txt y messages.txt
+            #R: -
         def cut_sales():
             f = open("mensajes.txt", "r")
             f2 = open("messages.txt", "r")
@@ -531,31 +656,23 @@ def main():
             else:
                 popup("Sales restarted")
 
+        #generate_report(): Genera un reporte de ventas
+            #E: -
+            #S: Crea un Tk que despliega las ventas hasta el momento
+            #R: -
         def generate_report():
             f = open("mensajes.txt", "r")
             f2 = open("messages.txt", "r")
             words = f.read()
             jump = 0
-            #ele = 0
             str_out = ""
-            #reading = False
-            #save = False
             params = []
             mat_out = []
-            #for i in range(len(words)):
-            #    if jump >= 2:
-            #        if words[i] != ".":
-            #            str_out += words[i]
-            #        else:
-            #            str_out += " "
-            #    elif words[i] == "\n":
-            #        jump += 1
             for x in range(2):
                 for i in range(len(words)):
                     if jump >= 2 and words[i] != "\n":
                         if words[i] == ".":
                             params += [str_out]
-                            #ele += 1
                             str_out = ""
                         elif words[i] != ".":
                             if words[i] != "/":
@@ -569,9 +686,7 @@ def main():
                             mat_out += [params]
                         params = []
                         str_out = ""
-                        #ele = 0
                         jump += 1
-                        #reading = True
                 params = []
                 jump = 0
                 words = f2.read()
@@ -603,8 +718,12 @@ def main():
             label.pack()
             report.mainloop()
 
+        #check_pw(): Verifica la contraseña que brinda el usuario
+            #E: -
+            #S: Revisa que la contraseña que se ingresó es la adecuada, de lo contrario genera un popup de error
+            #R: -
         def check_pw():
-            if password.get() == "acm1pt":
+            if password.get() == "acm3pd":
                 pw.destroy()
                 submit.destroy()
                 functions()
@@ -619,12 +738,13 @@ def main():
 
         back = Button(back_g, command=admin_s.destroy, relief=FLAT, width=9, height=3, text="Back",bg="red", fg="white", font=("Fixedsys", 8))
         back.place(x=20, y=280)
-        
-    #self.button_enter.place(x=410,y=210)
+
+    #Boton para cambiar de idioma
     button_lan = Button(root, image=lan, command=change_lan, relief=FLAT, width=60, height=30, bg="black")
     button_lan.image = lan
     button_lan.place(x=410,y=50)
 
+    #Boton para ingresar a pantalla de administrador
     button_admin = Button(root, image=admin, command=pw_screen, relief=FLAT, width=60, height=30, bg="black")
     button_admin.image = admin
     button_admin.place(x=410,y=550)
